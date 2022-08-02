a320432ebc1530018e8a0a5ddb74fee73924c779
Anilkumar18 <32846534+Anilkumar18@users.noreply.github.com>
2022-08-02 20:58:59 +0530

    updated version number as 2.2.17

--- wp-starter.php
+++ wp-starter.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gravity Forms WPMktgEngine Extension
 Description: This plugin requires the WPMKtgEngine or Genoo plugin installed before order to activate.
-Version: 2.2.16
+Version: 2.2.17
 Requires PHP: 7.1
 Author: Genoo LLC
 */
